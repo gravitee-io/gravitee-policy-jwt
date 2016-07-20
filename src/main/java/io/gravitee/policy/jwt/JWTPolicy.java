ee6d1fcbc039809fb04e0b2dec00d8658866f889
Alexandre Faria <alexandre.faria@decathlon.com>
2016-07-20 14:47:41 +0200

    Add possibility to cache validated JWT in order to avoid signature validation on each api call

--- src/main/java/io/gravitee/policy/jwt/JWTPolicy.java
+++ src/main/java/io/gravitee/policy/jwt/JWTPolicy.java
@@ -15,6 +15,7 @@
  */
 package io.gravitee.policy.jwt;
 
+import com.sun.corba.se.impl.protocol.POALocalCRDImpl;
 import io.gravitee.common.http.HttpHeaders;
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.gateway.api.ExecutionContext;
@@ -23,7 +24,13 @@ import io.gravitee.gateway.api.Response;
 import io.gravitee.policy.api.PolicyChain;
 import io.gravitee.policy.api.PolicyResult;
 import io.gravitee.policy.api.annotations.OnRequest;
+import io.gravitee.policy.jwt.exceptions.ValidationFromCacheException;
+import io.gravitee.resource.api.ResourceManager;
+import io.gravitee.resource.cache.Cache;
+import io.gravitee.resource.cache.CacheResource;
+import io.gravitee.resource.cache.Element;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.impl.DefaultClaims;
 import org.springframework.core.env.Environment;
 
 import java.io.ByteArrayInputStream;
@@ -34,6 +41,7 @@ import java.security.Key;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.RSAPublicKeySpec;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.regex.Matcher;
@@ -41,6 +49,10 @@ import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
 public class JWTPolicy {
+
+    /**
+     * Private JWT constants
+     */
     private static final String BEARER = "Bearer";
     private static final String ACCESS_TOKEN = "access_token";
     private static final String ISS = "iss";
@@ -52,6 +64,7 @@ public class JWTPolicy {
      * The associated configuration to this JWT Policy
      */
     private JWTPolicyConfiguration configuration;
+    private Cache cache;
 
     /**
      * Create a new JWT Policy instance based on its associated configuration
@@ -62,40 +75,133 @@ public class JWTPolicy {
         this.configuration = configuration;
     }
 
+
+
     @OnRequest
     public void onRequest(Request request, Response response, ExecutionContext executionContext, PolicyChain policyChain) {
         try {
-            final String authorization = request.headers().getFirst(HttpHeaders.AUTHORIZATION);
-            String jwt;
+            //1st extract the JWT to validate.
+            String jwt = extractJsonWebToken(request);
 
-            if (authorization != null) {
-                jwt = authorization.substring(BEARER.length()).trim();
-            } else {
-                jwt = request.parameters().get(ACCESS_TOKEN);
+            //2nd check if cache is enabled and if yes, check if JWT has already been validated.
+            if(this.configuration.isUseValidationCache()) {
+                try {
+                    validateTokenFromCache(executionContext, jwt);
+                }catch (ValidationFromCacheException e) {
+                    policyChain.failWith(PolicyResult.failure(e.getMessage()));
+                }
+            }
+            //3rd, if no cache is used, then just parse and validate it.
+            else {
+                validateJsonWebToken(executionContext, jwt);
             }
 
-            SigningKeyResolver resolver = new SigningKeyResolverAdapter() {
-
-                @Override
-                public Key resolveSigningKey(JwsHeader header, Claims claims) {
-
-                    String keyId = header.getKeyId(); //or any other field that you need to inspect
-                    final String iss = (String) claims.get(Claims.ISSUER);
-
-                    if (keyId == null || keyId.isEmpty()) {
-                        keyId = DEFAULT_KID;
-                    }
-
-                    Environment env = executionContext.getComponent(Environment.class);
-                    return parsePublicKey(env.getProperty(String.format(PUBLIC_KEY_PROPERTY, iss, keyId)));
-                }
-            };
-
-            final Jwt token = Jwts.parser().setSigningKeyResolver(resolver).parse(jwt);
+            //Finally continue the process...
             policyChain.doNext(request, response);
-        } catch (Exception e) {
+
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException e ) {
             policyChain.failWith(PolicyResult.failure(HttpStatusCode.UNAUTHORIZED_401, "Unauthorized"));
         }
+    }
+
+    /**
+     * Extract JWT from the request.
+     * First attempt to extract if from standard Authorization header.
+     * If none, then try to extract it from access_token query param.
+     * @param request Request
+     * @return String Json Web Token.
+     */
+    private String extractJsonWebToken(Request request) {
+        final String authorization = request.headers().getFirst(HttpHeaders.AUTHORIZATION);
+        String jwt;
+
+        if (authorization != null) {
+            jwt = authorization.substring(BEARER.length()).trim();
+        } else {
+            jwt = request.parameters().get(ACCESS_TOKEN);
+        }
+
+        return jwt;
+    }
+
+    private void validateTokenFromCache(ExecutionContext executionContext, String jwt) throws ValidationFromCacheException{
+
+        //Get Cache
+        CacheResource cacheResource = executionContext.getComponent(ResourceManager.class)
+                .getResource(this.configuration.getCacheName(), CacheResource.class);
+
+        if (cacheResource == null) {
+            throw new ValidationFromCacheException("No cache has been defined with name " + this.configuration.getCacheName());
+        }
+
+        cache = cacheResource.getCache();
+        if (cache == null) {
+            throw new ValidationFromCacheException("No cache named [ " + this.configuration.getCacheName() + " ] has been found.");
+        }
+
+        //Get token expiration date from cache.
+        Element cacheElement = cache.get(jwt);
+        Instant expiration;
+        if (cacheElement == null) {
+            //If no token found in cache then parse/validate/cache it
+            expiration = validateJsonWebToken(executionContext, jwt);
+            addJsonWebTokenToCache(jwt, expiration);
+        } else {
+            //If token (cache key) exists in cache, check expiration time (cache value).
+            expiration = (Instant) cacheElement.value();
+            if (Instant.now().isAfter(expiration)) {
+                throw new JwtException("Token expired!");
+            }
+        }
+    }
+
+    /**
+     * This method is used to validate the JWT Token.
+     * It will check the signature (by using the public RSA key linked to the JWT issuer)
+     * Then it will check the expiration date provided in the token.
+     * @param executionContext ExecutionContext used to retrieve the public RSA key.
+     * @param jwt String Json Web Token
+     * @return Instant expiration provided in the token (may be cached)
+     */
+    private Instant validateJsonWebToken(ExecutionContext executionContext, String jwt) {
+        SigningKeyResolver resolver = new SigningKeyResolverAdapter() {
+
+            @Override
+            public Key resolveSigningKey(JwsHeader header, Claims claims) {
+
+                String keyId = header.getKeyId(); //or any other field that you need to inspect
+                final String iss = (String) claims.get(Claims.ISSUER);
+
+                if (keyId == null || keyId.isEmpty()) {
+                    keyId = DEFAULT_KID;
+                }
+
+                Environment env = executionContext.getComponent(Environment.class);
+                return parsePublicKey(env.getProperty(String.format(PUBLIC_KEY_PROPERTY, iss, keyId)));
+            }
+        };
+
+        final Jwt token = Jwts.parser().setSigningKeyResolver(resolver).parse(jwt);
+        return ((DefaultClaims) token.getBody()).getExpiration().toInstant();
+    }
+
+    /**
+     * Just fill the cache with as key the JWT and as value the expiration date.
+     * @param jwt String Json Web Token
+     * @param expiration Instant
+     */
+    private void addJsonWebTokenToCache(String jwt, Instant expiration) {
+        cache.put(new Element() {
+            @Override
+            public Object key() {
+                return jwt;
+            }
+
+            @Override
+            public Object value() {
+                return expiration;
+            }
+        });
     }
 
     static RSAPublicKey parsePublicKey(String key) {
