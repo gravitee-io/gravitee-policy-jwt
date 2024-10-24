4b13ce0c77aff1519f3305c3f489d9b6d67a382b
Antoine CORDIER <antoine.cordier@graviteesource.com>
2024-10-24 17:55:02 +0100

    feat: make jwks url timeouts configurable
    see https://gravitee.atlassian.net/browse/APIM-7268

--- src/main/java/io/gravitee/policy/jwt/configuration/JWTPolicyConfiguration.java
+++ src/main/java/io/gravitee/policy/jwt/configuration/JWTPolicyConfiguration.java
@@ -43,6 +43,8 @@ public class JWTPolicyConfiguration implements PolicyConfiguration {
     private String userClaim;
     private String clientIdClaim;
     private boolean useSystemProxy;
+    private Integer connectTimeout;
+    private Long requestTimeout;
     private ConfirmationMethodValidation confirmationMethodValidation = new ConfirmationMethodValidation();
 
     @NoArgsConstructor
