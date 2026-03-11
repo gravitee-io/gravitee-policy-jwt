daead0e0495a4d6848b46f997eba756216a2a917
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-03-11 09:10:30 +0100

    fix: remove little perf improvement
    https://gravitee.atlassian.net/browse/APIM-13076

--- src/main/java/io/gravitee/policy/jwt/JWTPolicy.java
+++ src/main/java/io/gravitee/policy/jwt/JWTPolicy.java
@@ -226,11 +226,6 @@ public class JWTPolicy extends JWTPolicyV3 implements HttpSecurityPolicy, KafkaS
     }
 
     private Single<LazyJWT> fetchJWTToken(BaseExecutionContext ctx) {
-        Object lazyJwtFromContext = ctx.getAttribute(CONTEXT_ATTRIBUTE_JWT);
-        if (lazyJwtFromContext instanceof LazyJWT lazyJWT) {
-            return Single.just(lazyJWT);
-        }
-
         Optional<String> token = TokenExtractor.extract(ctx);
         if (token.isEmpty()) {
             return interruptUnauthorized(ctx, JWT_MISSING_TOKEN_KEY, new RuntimeException(ERROR_MSG_MISSING_TOKEN));
